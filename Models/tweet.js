521c21417b59909091fe3b3adb47daaba72a4cd4
Okoye Victor <okoyeebuka64@gmail.com>
2022-05-22 13:44:10 +0100

    Embedding ObjectId for Ref

--- Models/tweet.js
+++ Models/tweet.js
@@ -12,6 +12,8 @@ mongoose.connect('mongodb://localhost:27017/relationshipDemo', { useNewUrlParser
     })
 
 
+
+    // One To Millionzzzzz
 const  userSchema = new Schema({
     username: String,
     age: Number
@@ -20,13 +22,34 @@ const  userSchema = new Schema({
 const tweetSchema = new Schema({
     text: String,
     likes: Number,
-    
+    user: {
+        type: Schema.Types.ObjectId, ref: 'User'
+    }
 })
 
-const farmSchema = new mongoose.Schema({
-    name: String,
-    city: String,
-    products: [
-        {type: Schema.Types.ObjectId, ref:'Product'}
-    ]
-})
+// const farmSchema = new mongoose.Schema({
+//     name: String,
+//     city: String,
+//     products: [
+//         {type: Schema.Types.ObjectId, ref:'Product'}
+//     ]
+// })
+
+const User = mongoose.model('User', userSchema);
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+
+// const makeTweet = async ()=> {
+//     const user = await User.findOne({username: 'Chulo', age: 42})
+//     const tweet2 = new Tweet({text:'Look who is here now', likes:10})
+//     tweet2.user = user;
+//     tweet2.save();
+// }
+// makeTweet();
+
+const findTweet = async ()=>{
+ const t = await Tweet.find({}).populate('user',)
+ console.log(t);
+}
+
+findTweet();
