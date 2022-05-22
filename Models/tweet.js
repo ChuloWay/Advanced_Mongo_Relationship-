1765e1b17c92a38037a0b1656d83ae8da0ae1f69
Okoye Victor <okoyeebuka64@gmail.com>
2022-05-22 01:49:21 +0100

    Improved

--- Models/tweet.js
+++ Models/tweet.js
@@ -12,14 +12,17 @@ mongoose.connect('mongodb://localhost:27017/relationshipDemo', { useNewUrlParser
     })
 
 
-const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    season: {
-        type: String,
-        enum: ['Spring', 'Summer', 'Fall', 'Winter']
-    }
-});
+const  userSchema = new Schema({
+    username: String,
+    age: Number
+})
+
+const tweetSchema = new Schema({
+    text: String,
+    likes: Number,
+    
+})
+
 const farmSchema = new mongoose.Schema({
     name: String,
     city: String,
@@ -27,41 +30,3 @@ const farmSchema = new mongoose.Schema({
         {type: Schema.Types.ObjectId, ref:'Product'}
     ]
 })
-const Product = mongoose.model('Product', productSchema);
-const Farm = mongoose.model('Farm', farmSchema);
-
-
-
-// 1st collection
-// Product.insertMany([
-
-//     { name: ' Goddess Melon', price: 3.99, season: 'Summer' },
-//     { name: ' Sugar Baby Watermelon', price: 4.99, season: 'Summer' },
-//     {
-//         name: 'Asparagus', price: 3.59, season: 'Summer'
-//     }])
-
-
-// 2nd Collection where we ref products from the first collection through its objectId
-// const makeFarm = async () => {
-//     const farm =  new Farm({ name: 'Full Belly Farms', city: 'Guinda, CA'})
-//     const melon = await Product.findOne({name: ' Goddess Melon'})
-//     farm.products.push(melon)
-//    await farm.save();
-//     console.log(farm);
-// }
-// makeFarm();
-
-const addProduct = async () => {
-    const farm = await Farm.findOne({ name: 'Full Belly Farms'})
-    const watermelon = await Product.findOne({name: 'Asparagus'})
-    farm.products.push(watermelon)
-    farm.save()
-    console.log(farm);
-}
-
-// addProduct();
-
-Farm.findOne({ name: 'Full Belly Farms'})
-.populate('products')
-.then(farm => console.log(farm))
